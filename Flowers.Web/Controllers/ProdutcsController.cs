8aadcb2342637ae00f2b80e51eccb8ca35697fd6
Sergey Mishin <sergeysmishin@gmail.com>
2017-01-11 23:57:39 +0300

    Produtcs for users

---
+++ Flowers.Web/Controllers/ProdutcsController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace Flowers.Web.Controllers
+{
+    public class ProdutcsController : Controller
+    {
+        // GET: Produtcs
+		[Route("{type}")]
+        public ActionResult Index(string type)
+        {
+            return View();
+        }
+    }
+}
