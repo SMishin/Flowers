37659097e8cfef8400bd3c7669c6cc906004cc09
Sergey Mishin <sergeysmishin@gmail.com>
2017-01-21 17:03:28 +0300

    Improvements

--- Flowers.BL/Products/ProductType/ProductType.cs
+++ Flowers.BL/Products/ProductType/ProductType.cs
@@ -1,9 +1,25 @@
-﻿namespace Flowers.BL.Products.ProductType
+﻿using System.Collections.Generic;
+
+namespace Flowers.BL.Products.ProductType
 {
 	public enum ProductType
 	{
 		Undefined = -1,
 		Flowers = 0,
 		Toys = 1
+	}
+
+	public static class ProductTypeExtensions
+	{
+		private static readonly Dictionary<ProductType, string> Values = new Dictionary<ProductType, string>
+		{
+			{ProductType.Flowers,"Цветы"},
+			{ProductType.Toys,"Игрушки"},
+			{ProductType.Undefined,"Undefined"}
+		};
+		public static string ToFriendlyString(this ProductType me)
+		{
+			return Values[me];
+		}
 	}
 }
