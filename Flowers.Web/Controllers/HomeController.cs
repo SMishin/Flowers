aaca8b301094c416240d2ff0c6d6453050c4b575
Sergey Mishin <sergeysmishin@gmail.com>
2017-01-28 00:15:34 +0300

    Build script

--- Flowers.Web/Controllers/HomeController.cs
+++ Flowers.Web/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Web.Mvc;
 using Flowers.BL.Products;
 using Flowers.BL.Products.ProductType;
@@ -9,24 +7,25 @@ namespace Flowers.Web.Controllers
 {
 	public class HomeController : Controller
 	{
-		private readonly IProductReadOnlyStore _productReadOnlyStore;
+		//private readonly IProductReadOnlyStore _productReadOnlyStore;
 
-		public HomeController(IProductReadOnlyStore productReadOnlyStore)
+		//public HomeController(IProductReadOnlyStore productReadOnlyStore)
+		//{
+		//	_productReadOnlyStore = productReadOnlyStore;
+		//}
+
+		//public async Task<ActionResult> Index()
+		public ActionResult Index()
 		{
-			_productReadOnlyStore = productReadOnlyStore;
-		}
+			//var flowers = _productReadOnlyStore.GetWithMainImageAsync(ProductType.Flowers, 0, 12);
+			//var toys = _productReadOnlyStore.GetWithMainImageAsync(ProductType.Toys, 0, 12);
 
-		public async Task<ActionResult> Index()
-		{
-			var flowers = _productReadOnlyStore.GetWithMainImageAsync(ProductType.Flowers, 0, 12);
-			var toys = _productReadOnlyStore.GetWithMainImageAsync(ProductType.Toys, 0, 12);
-
-			await Task.WhenAll(flowers, toys);
+			//await Task.WhenAll(flowers, toys);
 
 			var data = new Dictionary<ProductType, Product[]>
 			{
-				{ProductType.Flowers, flowers.Result},
-				{ProductType.Toys, toys.Result}
+				//{ProductType.Flowers, flowers.Result},
+				//{ProductType.Toys, toys.Result}
 			};
 
 			return View(data);
