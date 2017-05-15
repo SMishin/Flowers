7fa138649ec228946bc665a76f51adfb71372b9d
Sergey Mishin <sergeysmishin@gmail.com>
2017-05-15 23:20:01 +0300

    update user data script

--- Flowers.DB/dbo/Data/Users.sql
+++ Flowers.DB/dbo/Data/Users.sql
@@ -31,7 +31,25 @@ begin
 			   ,'admin@admin.ru',
 			   'ADMIN@ADMIN.RU'
 			   )
-
+end
+else
+	begin
+		update [dbo].[Users]
+			   set
+			   [Email] = 'admin@admin.ru'
+			   ,[EmailConfirmed] = 0
+			   ,[PasswordHash] = 'AMQNEBRox6n8M1pwlBYaHZfKbrSDrIg/H+dzdOOJ8wcISwQc2ibsokq+VHCMauicDg=='
+			   ,[SecurityStamp] = '40a9ecc9-9934-46af-b4c4-413776c0c2f2'
+			   ,[PhoneNumber] = null
+			   ,[PhoneNumberConfirmed] = 0
+			   ,[TwoFactorEnabled]=0
+			   ,[LockoutEndDateUtc]=null
+			   ,[LockoutEnabled]=1
+			   ,[AccessFailedCount] =0 
+			   ,[UserName] = 'admin@admin.ru'
+			   ,[NormalizedUserName] ='ADMIN@ADMIN.RU'
+	
+			   where id= '21302f78-721f-428d-bd9b-e26f7fcc3f79'
 end
 
 
