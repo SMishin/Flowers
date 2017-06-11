3d2b6855a6e3387520dc8bebc5eee15872f389af
Sergey Mishin <sergeysmishin@gmail.com>
2017-06-11 15:42:27 +0300

    Empty results search view fix

--- Flowers.Core-Web/wwwroot/app/public/total-counter/template.jsx
+++ Flowers.Core-Web/wwwroot/app/public/total-counter/template.jsx
@@ -5,11 +5,11 @@ export default function template() {
 	let totalCount = this.state.totalCount,
 		lastNumber = totalCount % 10;
 
-	return <span>
+	return totalCount > 0 ? <span>
         Всего {totalCount} {' '}
 		{lastNumber === 1 && 'наименование'}
 		{lastNumber > 1 && lastNumber < 5 && 'наименования'}
 		{lastNumber === 0 || lastNumber >= 5 && 'наименований'}
-	</span>
+	</span> : null
 }
 
