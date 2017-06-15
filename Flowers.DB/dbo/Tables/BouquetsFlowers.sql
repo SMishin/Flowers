2d98ce00ea88bf1765c95c47ea939c87da63ad8d
Sergey Mishin <sergey.mishin@icmcapital.co.uk>
2017-06-15 19:45:43 +0300

    BouquetsFlowers table added

---
+++ Flowers.DB/dbo/Tables/BouquetsFlowers.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[BouquetsFlowers]
+(
+	[BouquetId]	int not null
+,	[FlowerId]	int not null
+,	constraint [FK_BouquetsFlowers_BouquetId] foreign key ([BouquetId]) references [Bouquets]([Id])
+,	constraint [FK_BouquetsFlowers_FlowerId] foreign key ([FlowerId]) references [Flowers]([Id])
+,	constraint [PK_BouquetsFlowers] primary key ([BouquetId],[FlowerId])
+)
